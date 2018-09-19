062d78d7defe751bab44f2827bf198da93ba9d65
tlaektmf <dazzel0826@gmail.com>
2018-09-19 10:33:39 +0900

    릴레이션 추가(학생, 교수, 과목), 클래스 설계

---
+++ app/src/main/java/kksy/konkuk_smart_ecampus/Student.java
@@ -0,0 +1,90 @@
+package kksy.konkuk_smart_ecampus;
+
+public class Student {
+    private String potalID; //ID
+    private String potalPW; //PW
+    private String studentID; //학번
+    private String department; //학과
+    private boolean beconCheck; //비콘 ON OFF 유무 default false
+    private boolean autoLoginCheck; //자동 로그인 체크 유므 default false
+    private String imgURL;//학생 사진
+
+    /*
+    constructor
+     */
+    public Student(){//default constructor
+
+    }
+    public Student(String potalID, String potalPW, String studentID, String department, String imgURL){
+
+        this.potalID = potalID;
+        this.potalPW = potalPW;
+        this.studentID = studentID;
+        this.department = department;
+        this.imgURL = imgURL;
+
+        this.autoLoginCheck=false;
+        this.beconCheck=false;
+    }
+
+    /*
+    get, set 함수
+     */
+    public String getPotalID() {
+        return potalID;
+    }
+
+    public void setPotalID(String potalID) {
+        this.potalID = potalID;
+    }
+
+    public String getPotalPW() {
+        return potalPW;
+    }
+
+    public void setPotalPW(String potalPW) {
+        this.potalPW = potalPW;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public boolean isBeconCheck() {
+        return beconCheck;
+    }
+
+    public void setBeconCheck(boolean beconCheck) {
+        this.beconCheck = beconCheck;
+    }
+
+    public boolean isAutoLoginCheck() {
+        return autoLoginCheck;
+    }
+
+    public void setAutoLoginCheck(boolean autoLoginCheck) {
+        this.autoLoginCheck = autoLoginCheck;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+
+}
