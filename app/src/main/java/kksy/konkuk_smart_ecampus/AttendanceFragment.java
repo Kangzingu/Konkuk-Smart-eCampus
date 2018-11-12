c3ff2e0801f4dd9bb6b4a6741a86544f7a8be6e9
Kangzingu <34128052+Kangzingu@users.noreply.github.com>
2018-11-13 01:44:12 +0900

    출석체크수정

--- app/src/main/java/kksy/konkuk_smart_ecampus/AttendanceFragment.java
+++ app/src/main/java/kksy/konkuk_smart_ecampus/AttendanceFragment.java
@@ -2,14 +2,24 @@ package kksy.konkuk_smart_ecampus;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static kksy.konkuk_smart_ecampus.AttendanceContentsAdapter.ABSENT;
@@ -40,10 +50,18 @@ public class AttendanceFragment extends Fragment {
      */
     private String subNum;
     private String stdId;
-
+    private String profId;
     AttendanceListAdapter adapter;
     RecyclerView recyclerView;
     List<AttendanceListAdapter.Item> data;
+
+    //징구의 디비 가져오기
+    FirebaseDatabase mdatabase;
+    DatabaseReference mdbRef;
+    Query query;
+    DataSnapshot currentObject;
+    int countAttendance;
+    ArrayList<AttendanceContentsAdapter.Item> contents;
 
     public AttendanceFragment() {
         // Required empty public constructor
@@ -69,8 +87,12 @@ public class AttendanceFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            subNum = getArguments().getString(ARG_SUBNUM);
+            //subNum = getArguments().getString(ARG_SUBNUM);
+            //지금 s2인데 출석부에 없어서 s1으로 일단 바꿔놓을게욤
+            subNum = "s184325";
             stdId = getArguments().getString(ARG_STDID);
+            //이거 바꿔야해용 이 과목의 프로페서 아이디가 필요해욤
+            profId = "p25787542";
         }
     }
 
@@ -86,7 +108,61 @@ public class AttendanceFragment extends Fragment {
         return view;
     }
 
-    public void initAttendance(){
+    public void initAttendance() {
+        data = new ArrayList<>();
+        contents = new ArrayList<>();
+        mdatabase = FirebaseDatabase.getInstance();
+        mdbRef = mdatabase.getReference("attendance");
+        //출석 가져왔죵
+        query = mdbRef.orderByKey().equalTo(subNum + "-" + profId);
+        countAttendance = 0;
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Iterator<DataSnapshot> iter = dataSnapshot.getChildren().iterator();
+                boolean isConditionOk = false;
+                while (iter.hasNext()) {
+                    currentObject = iter.next();
+                    if (currentObject.hasChild(stdId)) {
+                        iter = currentObject.child(stdId).getChildren().iterator();
+                        isConditionOk = true;
+                        break;
+                    }
+                }
+                while (iter.hasNext() && isConditionOk) {
+                    //내 학번에 해당하는 애 정보를 성공적으로 가져왔을때
+                    currentObject = iter.next();
+                    Iterator<DataSnapshot> childIter = currentObject.getChildren().iterator();
+                    while (childIter.hasNext()) {
+                        currentObject = childIter.next();
+                        countAttendance++;
+                        int state;
+                        switch (currentObject.child("state").getValue().toString()) {
+                            case "출석":
+                                state = ATTEND;
+                                break;
+                            case "지각":
+                                state = LATE;
+                                break;
+                            case "결석":
+                                state = ABSENT;
+                                break;
+                            default:/*이거 바꺼야해용*/
+                                state = ABSENT;
+                                break;
+                        }
+                        contents.add(new AttendanceContentsAdapter.Item(state, currentObject.child("date").getValue().toString()));
+                    }
+                }
+                showAttendance();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         /*
         - 진구
 
@@ -101,26 +177,43 @@ public class AttendanceFragment extends Fragment {
         - AttendanceContentsAdapter.Item : 출석 날짜(date)와 그 날짜의 출석 여부(state)
             state 는 ATTEND(출석), LATE(지각), ABSENT(결석)으로 구분
          */
-        data = new ArrayList<>();
+//        data = new ArrayList<>();
+//
+//        ArrayList<AttendanceContentsAdapter.Item> contents1 = new ArrayList<>();
+//        contents1.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.03(월)"));
+//        contents1.add(new AttendanceContentsAdapter.Item(LATE, "2018.09.05(수)"));
+//        data.add(new AttendanceListAdapter.Item(1, contents1));
+//
+//        ArrayList<AttendanceContentsAdapter.Item> contents2 = new ArrayList<>();
+//        contents2.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.10(월)"));
+//        contents2.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.12(수)"));
+//        data.add(new AttendanceListAdapter.Item(2, contents2));
+//
+//        ArrayList<AttendanceContentsAdapter.Item> contents3 = new ArrayList<>();
+//        contents3.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.17(월)"));
+//        contents3.add(new AttendanceContentsAdapter.Item(ABSENT, "2018.09.19(수)징구쨩"));
+//        data.add(new AttendanceListAdapter.Item(3, contents3));
+         }
 
-        ArrayList<AttendanceContentsAdapter.Item> contents1 = new ArrayList<>();
-        contents1.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.03(월)"));
-        contents1.add(new AttendanceContentsAdapter.Item(LATE, "2018.09.05(수)"));
-        data.add(new AttendanceListAdapter.Item(1, contents1));
+    public void showAttendance() {
 
-        ArrayList<AttendanceContentsAdapter.Item> contents2 = new ArrayList<>();
-        contents2.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.10(월)"));
-        contents2.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.12(수)"));
-        data.add(new AttendanceListAdapter.Item(2, contents2));
-
-        ArrayList<AttendanceContentsAdapter.Item> contents3 = new ArrayList<>();
-        contents3.add(new AttendanceContentsAdapter.Item(ATTEND, "2018.09.17(월)"));
-        contents3.add(new AttendanceContentsAdapter.Item(ABSENT, "2018.09.19(수)"));
-        data.add(new AttendanceListAdapter.Item(3, contents3));
+        ArrayList<AttendanceContentsAdapter.Item> weeklyContents[] = new ArrayList[countAttendance/2];
+        for(int i=0;i<countAttendance/2;i++) {
+            weeklyContents[i]=new ArrayList<>();
+        }
+        for(int i=0;i<countAttendance;i++){
+            weeklyContents[i/2].add(contents.get(i));
+        }
+        for(int i=0;i<countAttendance/2;i++){
+            data.add(new AttendanceListAdapter.Item(i+1, weeklyContents[i]));
+        }
+        //data.add(new AttendanceListAdapter.Item(countAttendance, contents));
+        adapter = new AttendanceListAdapter(getContext(), data);
+        recyclerView.setAdapter(adapter);
+        //Toast.makeText(getActivity(), currentObject.getKey().toString(), Toast.LENGTH_SHORT).show();
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         adapter = new AttendanceListAdapter(getContext(), data);
         recyclerView.setAdapter(adapter);
     }
-
 }
